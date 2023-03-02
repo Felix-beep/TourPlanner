a71d50017e0acbfa0eb76d69d3e108179d63b620
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2023-03-02 11:07:59 +0100

    created projects
    added initial projects including log4net logging

---
+++ TourPlanner/TourPlanner/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace TourPlanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(MainWindow));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            XmlConfigurator.Configure(new FileInfo("log4net.cfg"));
+
+            log.Info("Logging configured.");
+        }
+
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            log.Info("Clicked MenuItem File.New!");
+        }
+    }
+}
