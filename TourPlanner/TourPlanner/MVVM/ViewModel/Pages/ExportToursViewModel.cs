94ef6aecf9637d801e3780c8670b5f1eabdbdedb
Felix Kainz <Fuxi.Kainz@gmail.com>
2023-04-06 20:31:45 +0200

    Added Content for other Views

--- TourPlanner/TourPlanner/MVVM/ViewModel/Pages/ExportToursViewModel.cs
+++ TourPlanner/TourPlanner/MVVM/ViewModel/Pages/ExportToursViewModel.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TourPlanner.Core;
+using TourPlanner.Models;
 
 namespace TourPlanner.MVVM.ViewModel
 {
     public class ExportToursViewModel : ObservableObject
     {
+        public List<Tour> Items { get; set; }
 
+        public ExportToursViewModel()
+        {
+            Items = new()
+            {
+                new Tour(1, "Hello", "some description"),
+                new Tour(2, "Hallo", "some description"),
+                new Tour(3, "Tschau", "some description")
+            };
+        }
     }
 }
