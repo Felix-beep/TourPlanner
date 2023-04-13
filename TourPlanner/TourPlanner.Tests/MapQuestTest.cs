702ccc2ed7c74562379ac1865a0c65cbfd919d88
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2023-04-13 10:12:36 +0200

    created basic mapquest client class

---
+++ TourPlanner/TourPlanner.Tests/MapQuestTest.cs
@@ -0,0 +1,14 @@
+﻿using TourPlanner.DAL;
+
+namespace TourPlanner.Tests
+{
+    public class MapQuestTest
+    {
+        [Test]
+        public void Test()
+        {
+            var client = new MapQuestClient("");
+            client.Request();
+        }
+    }
+}
