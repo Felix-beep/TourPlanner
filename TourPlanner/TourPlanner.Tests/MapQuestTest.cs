a1abbb22f2f1031b148ae91f85f7490b1c3ba2df
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2023-04-13 10:30:26 +0200

    hiding api keys

--- TourPlanner/TourPlanner.Tests/MapQuestTest.cs
+++ TourPlanner/TourPlanner.Tests/MapQuestTest.cs
@@ -1,13 +1,24 @@
-﻿using TourPlanner.DAL;
+﻿using Microsoft.Extensions.Configuration;
+using TourPlanner.DAL;
 
 namespace TourPlanner.Tests
 {
     public class MapQuestTest
     {
+        IConfiguration config;
+
+        [OneTimeSetUp]
+        public void LoadConfig()
+        {
+            config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
         [Test]
         public void Test()
         {
-            var client = new MapQuestClient("");
+            var client = new MapQuestClient(config.GetSection("ApiKeys")["MapQuestKey"]);
             client.Request();
         }
     }
