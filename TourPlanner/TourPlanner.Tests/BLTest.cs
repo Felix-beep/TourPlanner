ad1aa9f4ab7cd94af5e08ecf6dc398b7b9b64c74
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2023-06-04 17:37:13 +0200

    added asserts to the bl unit tests

--- TourPlanner/TourPlanner.Tests/BLTest.cs
+++ TourPlanner/TourPlanner.Tests/BLTest.cs
@@ -13,12 +13,26 @@ namespace TourPlanner.Tests
         }
 
         [Test]
-        public async Task ExportTest() 
+        public async Task ExportTest()
         {
+            // These samples get exported:
+            //var samples = new Tour[]
+            //{
+            //    new Tour(1, "Hello Subject1", "description1", new List<TourLog>()),
+            //    new Tour(2, "Subject2", "description2", new List<TourLog>()),
+            //    new Tour(3, "Subject3", "description3", new List<TourLog>()),
+            //    new Tour(4, "Food", "Food", new List<TourLog>()),
+            //};
+
             var repo = new ConnectionModeFactory(null, new MemoryTourRepository());
 
             var bl = new BackendLogic(repo);
             await bl.ExportToursAsync(await bl.GetAllToursAsync());
+
+            Assert.That(File.Exists("Hello Subject1.csv"));
+            Assert.That(File.Exists("Subject2.csv"));
+            Assert.That(File.Exists("Subject3.csv"));
+            Assert.That(File.Exists("Food.csv"));
         }
 
         [Test]
@@ -28,6 +42,8 @@ namespace TourPlanner.Tests
 
             var bl = new BackendLogic(repo);
             await bl.ImportToursAsync(new string[] { "Subject2.csv" });
+
+            Assert.That((await repo.GetRepo().GetToursAsync()).Count(), Is.EqualTo(4 + 1));
         }
     }
 }
